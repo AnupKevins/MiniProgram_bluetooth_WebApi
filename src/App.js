5726259b870059662a4188cb8ca6c47f33d1c894
Anup <anup.sahu@thoughtworks.com>
2024-02-09 12:43:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const BluetoothBatteryReader = () => {
+  const [batteryInfo, setBatteryInfo] = useState('');
+  
+  const readBatteryLevel = () => {
+    if (!('bluetooth' in navigator)) {
+      setBatteryInfo('Bluetooth API not supported.');
+      return;
+    }
+
+    navigator.bluetooth.requestDevice({
+      filters: [{
+        services: ['battery_service']
+      }]
+    })
+      .then(device => device.gatt.connect())
+      .then(server => server.getPrimaryService('battery_service'))
+      .then(service => service.getCharacteristic('battery_level'))
+      .then(characteristic => characteristic.readValue())
+      .then(value => {
+        setBatteryInfo(`Battery percentage is ${value.getUint8(0)}.`);
+      })
+      .catch(error => {
+        setBatteryInfo(error.toString());
+      });
+  };
+
+  useEffect(() => {
+    // Clean up any Bluetooth connections or resources on component unmount
+    return () => {
+      // You may need to handle disconnecting from Bluetooth here
+      // if it's necessary for your use case
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <button onClick={readBatteryLevel}>Read Bluetooth device's<br />battery level</button>
+      </p>
+      <p id="target">{batteryInfo}</p>
+      <p>
+        <small>Based on code snippets from <a href="https://developers.google.com/web/updates/2015/07/interact-with-ble-devices-on-the-web" target="_blank" rel="noopener">Google Developers</a>.</small>
+      </p>
     </div>
   );
-}
+};
 
-export default App;
+export default BluetoothBatteryReader;
